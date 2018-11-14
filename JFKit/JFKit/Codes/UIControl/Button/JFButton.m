a1ba59b431ba0c109e3dcdbcc45d2ef2b54a00b2
JoyFate <joyfate1@gmail.com>
2018-11-14 20:39:07 +0800

    JFButton 上图下文字 drawRect 修改

--- JFKit/JFKit/Codes/UIControl/Button/JFButton.m
+++ JFKit/JFKit/Codes/UIControl/Button/JFButton.m
@@ -75,13 +75,11 @@
     if (self.buttonImageType == JFButtonType_ImageTop) {
         CGSize imageSize = self.imageView.frame.size;
         CGSize titleSize = self.titleLabel.frame.size;
-        CGSize textSize = [self.titleLabel.text sizeWithAttributes:@{NSFontAttributeName:self.titleLabel.font}];
-        CGSize frameSize = CGSizeMake(ceilf(textSize.width), ceilf(textSize.height));
-        if (titleSize.width + 0.5 < frameSize.width) {
-            titleSize.width = frameSize.width;
-        }
-        self.imageEdgeInsets = UIEdgeInsetsMake(- (self.titleImageMargin - imageSize.height), 0.0, 0.0, - titleSize.width);
-        self.titleEdgeInsets = UIEdgeInsetsMake(0, - imageSize.width, - (self.titleImageMargin - titleSize.height), 0);
+        
+        self.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+        
+        self.titleEdgeInsets = UIEdgeInsetsMake(imageSize.width + self.titleImageMargin/2, -imageSize.width, 0, 0);
+        self.imageEdgeInsets = UIEdgeInsetsMake(0, 0, titleSize.width + self.titleImageMargin/2, -titleSize.width);
     }
     else if (self.buttonImageType == JFButtonType_ImageRight) {
         CGSize imageSize = self.imageView.frame.size;
